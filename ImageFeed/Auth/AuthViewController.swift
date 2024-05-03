97ca9ad86520773d7be49929fd8924f20163738a
Aleksandr Salagubov <salagubovalex@yandex-team.ru>
2024-05-03 16:03:18 +0300

    sprint_10: Added WebViewViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Alexander Salagubov on 02.05.2024.
+//
+
+import Foundation
+import UIKit
+
+final class AuthViewController: UIViewController {
+
+  private let ShowWebViewSegueIdentifier = "ShowWebView"
+
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Alexander Salagubov on 03.05.2024.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+
+  @IBOutlet weak var webView: WKWebView!
+
+  @IBAction func didTapBackButton(_ sender: Any) {
+  }
+  
+}
